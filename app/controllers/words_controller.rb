7178df5267642d26ad07084524ba2ae3996156df
kajigii <wata15708@gmail.com>
2023-11-04 18:10:10 +0900

    メッセージ送信機能の実装

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,22 @@
 class WordsController < ApplicationController
   def index
+    @word = Word.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @word = @room.words.new(word_params)
+    if @word.save
+      redirect_to room_words_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'words/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :words, only: [:index, :create]
+  end
 end
