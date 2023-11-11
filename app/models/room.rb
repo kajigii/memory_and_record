eae6682e3c9c8cda0f991b0d571650aaa951f28a
kajigii <wata15708@gmail.com>
2023-11-11 22:49:46 +0900

    roomの削除機能追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :room_users, dependent: :destroy
   has_many :words
 
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'words/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :words
   end
 end
