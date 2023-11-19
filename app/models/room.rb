8083d6a314b6d50714c3670b3dac90d708240d1a
kajigii <wata15708@gmail.com>
2023-11-20 02:36:17 +0900

    モデル再作成、投稿・詳細・編集画面修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
   has_many :room_users, dependent: :destroy
+  has_many :users, through: :room_users
   has_many :words
 
   validates :name, presence: true
